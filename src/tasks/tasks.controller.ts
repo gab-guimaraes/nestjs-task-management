c05eb99edd1ee079e260e40083a17107f9c1218d
Gabriel Guimaraes <dedito182@gmail.com>
2021-03-30 00:31:12 -0300

    creating api with nest js

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Delete, Param, Patch } from '@nestjs/common/decorators/http';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task, TaskStatus } from './tasks.model';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+  constructor(private taskService: TasksService) {} // nest.js dependency injection
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.taskService.getAllTasks();
+  }
+
+  @Get('/:id')
+  getTaskById(@Param('id') id: string) {
+    return this.taskService.getTaskById(id);
+  }
+
+  @Delete('/:id')
+  deleteTaskById(@Param('id') id: string) {
+    return this.taskService.deleteTask(id);
+  }
+
+  @Post()
+  createTask(@Body() createTaskDto: CreateTaskDto){
+    return this.taskService.createTask(createTaskDto);
+  }
+
+  @Patch('/:id/status') 
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.taskService.updateTaskStatus(id, status);
+  }
+}
